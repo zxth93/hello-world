049da0e2a405cd4bbe0f592e4743de080a77ed0a
zxth93 <zhangruiscu@foxmail.com>
2016-09-06 21:04:23 +0800

    commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by R Z on 16/9/6.
+//  Copyright © 2016年 R Z. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
